725aef14e017127b20eb437efadf380c28bc234f
Pothina Menaka Nagasai <menakans.9@gmail.com>
2025-11-12 17:04:23 -0600

    Initial Flask app commit

---
+++ project_v2/project_v2/lms_v2/run.py
@@ -0,0 +1,21 @@
+from flask import redirect, session, url_for
+from flask_login import logout_user, current_user
+from app import create_app
+
+app = create_app()
+
+
+@app.route("/")
+def default():
+    return redirect(url_for("member.login"))
+
+
+@app.route("/logout")
+def logout():
+    role = current_user.role
+    logout_user()
+    return redirect(url_for(f"{role}.login"))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
